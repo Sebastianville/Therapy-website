268f1358082a62d8d1b800c07bcc3bc34ced9e58
Sebastianville <sebastian.villegas2623@gmail.com>
2025-03-23 22:04:36 -0400

    now making the frontend talking to the backend

--- therapy-propject/vite.config.js
+++ therapy-propject/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite' 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        // This is where we are going to put the netlify link
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
